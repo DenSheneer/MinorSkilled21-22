3312a70e6193a7d49b88bb75a38de4eab918a02e
Denise <mail.deniseg@gmail.com>
2022-06-17 15:36:27 +0200

    Added slideshow scenes as a variation of sceneEvent

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,10 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
